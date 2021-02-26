c62c7b4de4a18811ee75b66497671cb2686f196a
German Espinoza <g.espinozat@pucp.pe>
2021-02-25 21:09:32 -0500

    Test sharedlib 1

---
+++ vars/mavenCompile.groovy
@@ -0,0 +1,27 @@
+def call(givenConfig = [:]) {
+    
+    def defaultConfig = [
+        "maven": [
+            "skipTests": true
+        ]
+    ]
+
+    def effectiveConfig merge(defaultConfig, givenConfig)
+    
+    node {
+
+        tools {
+            maven "M3"
+        }
+
+        if (effectiveConfig.maven.skipTests) {
+            stage("Clean and build skipping tests") {
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+        } else {
+            stage("Clean and build") {
+                sh "mvn -DskipTests clean package"
+            }
+        }
+    }
+}
